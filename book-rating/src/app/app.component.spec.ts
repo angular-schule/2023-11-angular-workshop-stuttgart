872fcb494c5b5dcf02664c6c7f3478854ae83fe1
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-11-20 10:46:33 +0100

    .overrideComponent

--- book-rating/src/app/app.component.spec.ts
+++ book-rating/src/app/app.component.spec.ts
@@ -1,11 +1,26 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { Component } from '@angular/core';
+import { DashboardComponent } from './books/dashboard/dashboard.component';
+
+@Component({
+  selector: 'app-dashboard',
+  template: '😃😃😃',
+  standalone: true
+})
+export class DummyDashboardComponent {
+}
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
-    }).compileComponents();
+    })
+    .overrideComponent(AppComponent, {
+      remove: { imports: [DashboardComponent] },
+      add: { imports: [DummyDashboardComponent] }
+    })
+    .compileComponents();
   });
 
   it(`should have the 'Book Rating' title`, () => {
