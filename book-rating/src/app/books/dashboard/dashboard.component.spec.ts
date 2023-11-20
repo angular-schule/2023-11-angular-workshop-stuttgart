4b84d4d82006d90f3c53096d62c9d6a81478d2c3
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-11-20 10:37:31 +0100

    fix unit tests

--- book-rating/src/app/books/dashboard/dashboard.component.spec.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.spec.ts
@@ -11,7 +11,6 @@ describe('DashboardComponent', () => {
       imports: [DashboardComponent]
     })
     .compileComponents();
-    
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
