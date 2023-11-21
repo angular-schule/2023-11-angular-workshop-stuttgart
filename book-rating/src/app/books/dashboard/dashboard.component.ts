1dfab0cbb58a58b4d76ae600b27ee293b7d827be
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-11-21 10:01:24 +0100

    new book-create component

--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -3,11 +3,12 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { BookComponent } from '../book/book.component';
 import { Book } from '../shared/book';
 import { BookRatingService } from '../shared/book-rating.service';
+import { BookCreateComponent } from '../book-create/book-create.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [BookComponent],
+  imports: [BookComponent, BookCreateComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
   // changeDetection: ChangeDetectionStrategy.OnPush
