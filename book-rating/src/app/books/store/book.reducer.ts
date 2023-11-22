21d0be43a3fac671d2adcf29da6207037b945ed0
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-11-22 15:13:14 +0100

    ngrx: reducer

--- book-rating/src/app/books/store/book.reducer.ts
+++ book-rating/src/app/books/store/book.reducer.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @ngrx/on-function-explicit-return-type */
 import { createFeature, createReducer, on } from '@ngrx/store';
 import { BookActions } from './book.actions';
 import { Book } from '../shared/book';
@@ -16,9 +17,24 @@ export const initialState: State = {
 
 export const reducer = createReducer(
   initialState,
-  on(BookActions.loadBooks, state => state),
-  on(BookActions.loadBooksSuccess, (state, action) => state),
-  on(BookActions.loadBooksFailure, (state, action) => state),
+
+  on(BookActions.loadBooks, state => ({
+    ...state,
+    loading: true
+  })),
+
+  on(BookActions.loadBooksSuccess, (state, action) => ({
+    ...state,
+    loading: false,
+    books: action.data
+  })),
+
+
+  on(BookActions.loadBooksFailure, (state) => ({
+    ...state,
+    loading: false,
+    book: []
+  })),
 );
 
 export const bookFeature = createFeature({
