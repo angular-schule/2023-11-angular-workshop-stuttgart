5d0a3508a5cdef03c73f703ded7e7ac15d4ba8a9
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-11-22 13:27:43 +0100

    rxjs: takeUntilDestroyed()

--- rxjs-playground/src/app/exercises/unsubscribe/unsubscribe.component.ts
+++ rxjs-playground/src/app/exercises/unsubscribe/unsubscribe.component.ts
@@ -1,17 +1,16 @@
 import { Component, OnDestroy } from '@angular/core';
 import { Subject, ReplaySubject, timer, Subscription, takeWhile, takeUntil } from 'rxjs';
 import { HistoryComponent } from '../../shared/history/history.component';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop'
 
 @Component({
   templateUrl: './unsubscribe.component.html',
   standalone: true,
   imports: [HistoryComponent]
 })
-export class UnsubscribeComponent implements OnDestroy {
+export class UnsubscribeComponent {
 
   logStream$ = new ReplaySubject<string | number>();
-
-  destroyed = false;
 
   /**
    * Öffne die Browser-Console: Dort siehst Du den Output eines Observables, das jede Sekunde einen Wert generiert.
@@ -30,7 +29,7 @@ export class UnsubscribeComponent implements OnDestroy {
 
       /******************************/
 
-      takeWhile(() => !this.destroyed)
+      takeUntilDestroyed()
 
 
       /******************************/
@@ -40,10 +39,6 @@ export class UnsubscribeComponent implements OnDestroy {
       error: err => this.log('❌ ERROR: ' + err),
       complete: () => this.log('✅ COMPLETE')
     });
-  }
-
-  ngOnDestroy() {
-    this.destroyed = true;
   }
 
   log(msg: string | number) {
