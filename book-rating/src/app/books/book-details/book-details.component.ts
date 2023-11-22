56754d803445e9fc8440238b05f1ee711ff124ea
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-11-22 14:15:27 +0100

    rxjs: concatMap

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, RouterLink } from '@angular/router';
-import { map, mergeMap } from 'rxjs';
+import { concatMap, map, mergeMap } from 'rxjs';
 import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
@@ -17,7 +17,7 @@ export class BookDetailsComponent {
 
   book$ = inject(ActivatedRoute).paramMap.pipe(
     map(paramMap => paramMap.get('isbn') || ''),
-    mergeMap(isbn => this.bookStore.getSingleBook(isbn))
+    concatMap(isbn => this.bookStore.getSingleBook(isbn))
   );
 
 }
